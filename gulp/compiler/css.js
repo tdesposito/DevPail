9d0eb5e1e3a2ba45f64db97854e2058e09506e1f
Todd Esposito <todd@toddesposito.com>
2021-12-27 14:19:47 -0600

    Add css and js compilers

---
+++ gulp/compiler/css.js
@@ -0,0 +1,32 @@
+// This file is part of DevPail (https://github.com/tdesposito/DevPail)
+// Copyright(C) Todd D.Esposito 2021.
+// Distributed under the MIT License(see https://opensource.org/licenses/MIT).
+
+
+exports.install = [
+]
+
+
+exports.build = (compiler) => {
+}
+
+
+exports.dev = (gulp, compiler, bs) => {
+    function compile_css(done) {
+        gulp.src(`${source}/**/*.css`, {sourcemaps: true})
+            .pipe(rename({ extname: '.min.css' }))
+            .pipe(gulp.dest(target, { sourcemaps: '.' }))
+            .pipe(bs.stream())
+        done()
+    }
+    const rename = require('gulp-rename')
+    const source = compiler.source || 'src/css'
+    const target = compiler.target || 'dev/static/css'
+    return gulp.watch([`${source}/**/*.css`],
+        {
+            ignoreInitial: false,
+            usePolling: true,
+        },
+        compile_css
+    )
+}
---
+++ gulp/compiler/javascript.js
@@ -0,0 +1,32 @@
+// This file is part of DevPail (https://github.com/tdesposito/DevPail)
+// Copyright(C) Todd D.Esposito 2021.
+// Distributed under the MIT License(see https://opensource.org/licenses/MIT).
+
+
+exports.install = [
+]
+
+
+exports.build = (compiler) => {
+}
+
+
+exports.dev = (gulp, compiler, bs) => {
+    function compile_js(done) {
+        gulp.src(`${source}/**/*.js`, {sourcemaps: true})
+            .pipe(rename({ extname: '.min.js' }))
+            .pipe(gulp.dest(target, { sourcemaps: '.' }))
+            .pipe(bs.stream())
+        done()
+    }
+    const rename = require('gulp-rename')
+    const source = compiler.source || 'src/js'
+    const target = compiler.target || 'dev/static/js'
+    return gulp.watch([`${source}/**/*.js`],
+        {
+            ignoreInitial: false,
+            usePolling: true,
+        },
+        compile_js
+    )
+}
