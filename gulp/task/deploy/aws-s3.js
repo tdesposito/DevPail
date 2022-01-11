599d0240fd0919da977a7b08ef307966f51ee56b
Todd Esposito <todd@toddesposito.com>
2022-01-11 10:02:27 -0600

    Added s3 setup feature.
    Naming and executing a task named like "*:setup" will create your hosting bucket.

--- gulp/task/deploy/aws-s3.js
+++ gulp/task/deploy/aws-s3.js
@@ -7,23 +7,35 @@ exports.packages = [
 ]
 
 
-exports.task = (gulp, task) => {
+exports.task = (gulp, cfg, name) => {
     function sync_to_s3(done) {
-        const cmd = `aws s3 sync build/ s3://${task.bucket} --delete --acl public-read`
-        require('child_process').execSync(cmd, { shell: '/bin/bash', stdio: 'inherit'})
+        const cmd = `aws s3 sync build/ s3://${cfg.bucket} --delete --acl public-read`
+        execSync(cmd, { shell: '/bin/bash', stdio: 'inherit'})
         done()
     }
 
     function invalidate_cdn(done) {
-        const cmd = `aws cloudfront create-invalidation --distribution-id ${task.distribution} --paths "/*"`
-        require('child_process').execSync(cmd, { shell: '/bin/bash', stdio: 'inherit' })
+        const cmd = `aws cloudfront create-invalidation --distribution-id ${cfg.distribution} --paths "/*"`
+        execSync(cmd, { shell: '/bin/bash', stdio: 'inherit' })
         done()
     }
 
-    const tasks = ['build', sync_to_s3]
-    if (task.distribution) {
-        tasks.push(invalidate_cdn)
+    function setup_s3(done) {
+        const opts = cfg.createOptions || []
+        const cmdopts = opts.map(opt => `--${opt}`).join(' ')
+        const cmd = `aws s3api create-bucket --bucket ${cfg.bucket} ${cmdopts}`
+        execSync(cmd, { shell: '/bin/bash', stdio: 'inherit' })
+        done()
     }
 
-    return gulp.series(...tasks)
+    const { execSync } = require('child_process')
+    if (name.endsWith(':setup')) {
+        return setup_s3
+    } else {
+        const tasks = ['build', sync_to_s3]
+        if (cfg.distribution) {
+            tasks.push(invalidate_cdn)
+        }
+        return gulp.series(...tasks)
+    }
 }
